48c2fc8f2b7b300eb5f9a7e874ee2e2aec3c024b
Masiu121 <maks.kapolka@gmail.com>
2020-10-14 18:41:14 +0200

    Added location

---
+++ LocationData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourierTycoon
+{
+    class LocationData
+    {
+        string city;
+
+        public LocationData setLocation(string city)
+        {
+            this.city = city;
+            return this;
+        }
+    }
+}
