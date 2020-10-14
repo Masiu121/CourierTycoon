b9c5bee8d94dcca54ca65a927657cb20dd1be073
Masiu121 <maks.kapolka@gmail.com>
2020-10-14 17:36:38 +0200

    Added data base

---
+++ DataBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using CourierTycoon.Components;
+
+namespace CourierTycoon
+{
+    class DataBase
+    {
+        List<ParcelLocker> lockers = new List<ParcelLocker>();
+    }
+}
