c0277aea98e8f26e94fce32624ba1586a570d88c
Masiu121 <maks.kapolka@gmail.com>
2020-10-14 18:41:04 +0200

    Added hub

---
+++ Components/Hub.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourierTycoon.Components
+{
+    class Hub
+    {
+        LocationData location;
+        int packs;
+        int trucksCount;
+        int vansCount;
+
+        public Hub(string location)
+        {
+            this.location.setLocation(location);
+        }
+    }
+}
