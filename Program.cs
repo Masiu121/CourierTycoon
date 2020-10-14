11659e77c2906ddb834ea45523e6ea409b7418d7
Masiu games <masiugames@gmail.com>
2020-10-14 16:26:45 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CourierTycoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
