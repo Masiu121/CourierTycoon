88a757b94b4e84e48e994cd91c23e5b48fb96697
Masiu121 <maks.kapolka@gmail.com>
2020-10-16 21:06:07 +0200

    Updated hub

--- Components/Hub.cs
+++ Components/Hub.cs
@@ -7,13 +7,33 @@ namespace CourierTycoon.Components
     class Hub
     {
         LocationData location;
-        int packs;
-        int trucksCount;
-        int vansCount;
+        List<Pack> packs;
+        List<Truck> trucks;
+        List<Van> vans;
 
         public Hub(string location)
         {
             this.location.setLocation(location);
+            trucks = new List<Truck>();
+            vans = new List<Van>();
+        }
+
+        public void addTruck(int capacity)
+        {
+            Truck truck = new Truck(this, capacity);
+            trucks.Add(truck);
+        }
+
+        public void addVan(int capacity)
+        {
+            Van van = new Van(this, capacity);
+            vans.Add(van);
+        }
+
+        public void addParcel(ParcelLocker parcelLocker, Client client)
+        {
+            Pack pack = new Pack(client, parcelLocker);
+            packs.Add(pack);
         }
     }
 }
