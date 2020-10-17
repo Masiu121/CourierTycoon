6bd4620f74a1802b3f0578d5c0292a3170bf35e2
Masiu121 <maks.kapolka@gmail.com>
2020-10-17 10:15:41 +0200

    Updated location data

--- LocationData.cs
+++ LocationData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CourierTycoon.Components;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,11 +9,18 @@ namespace CourierTycoon
     {
         string city;
         string platePrefix;
+        List<ParcelLocker> lockers = new List<ParcelLocker>();
 
         public LocationData setLocation(string city)
         {
             this.city = city;
             return this;
+        }
+
+        public void addLocker(int capA, int capB, int capC)
+        {
+            ParcelLocker locker = new ParcelLocker(10, 10, 10);
+            lockers.Add(locker);
         }
     }
 }
