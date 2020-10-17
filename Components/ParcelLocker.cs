3f04f345fc63deaa786fefb933d913e023ffea70
Masiu121 <maks.kapolka@gmail.com>
2020-10-17 10:15:32 +0200

    Updated parcel locker

--- Components/ParcelLocker.cs
+++ Components/ParcelLocker.cs
@@ -10,12 +10,24 @@ namespace CourierTycoon.Components
         int capacityB;
         int capacityC;
         int id;
+        List<Pack> packs;
 
         public ParcelLocker(int capA, int capB, int capC)
         {
             capacityA = capA;
             capacityB = capB;
             capacityC = capC;
+            packs = new List<Pack>();
+        }
+
+        public void addPack(Pack pack)
+        {
+            packs.Add(pack);
+        }
+
+        public void removePack(Pack pack)
+        {
+            packs.Remove(pack);
         }
     }
 }
